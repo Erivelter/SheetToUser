388aefc545385ad240b3a1b48a178537cc315c90
Erivelter <eriveltervilasboas@hotmail.com>
2024-10-20 01:33:33 -0300

    made_csv

--- app/app.py
+++ app/app.py
@@ -1,16 +1,18 @@
 from flask import Flask, jsonify, request, send_from_directory, render_template
 import os
-from flask_sqlalchemy import SQLAlchemy
+import pandas as pd
+import json
+
+from model import  user
 Diretorio="C:\\Users\\erive\\OneDrive\\Documentos\\programação\\Study\\save_flask\\output"
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////user.db'
-# db = SQLAlchemy(app)
-# db.create_all()
-@app.route("/user",methods=["GET"])
-def mostar_user():
-    pass
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////user.db'
 
+db.create_all()
+csv_file=""
 
+with app.app_context():
+    db.create_all()
 
 @app.route("/file", methods=["GET"])
 def lista_arquivos():
@@ -34,13 +36,44 @@ def get_file(name_file):
 @app.route("/file", methods=["POST"])
 def post_arquivo():
     arquivo = request.files.get("uploaderFile")
-
+    global csv_file
     print(arquivo)
     nome_do_arquivo = arquivo.filename
     arquivo.save(os.path.join(Diretorio, nome_do_arquivo ))
+    csv_file = nome_do_arquivo
+    return '', 201
+
+
+@app.route("/get_csv", methods=["GET"])
+def get_user():
+    print(csv_file)
+    if csv_file is None:
+        return jsonify({"error": "Arquivo CSV não especificado"}), 400
+
+    try:
+        df = pd.read_csv(os.path.join(Diretorio, csv_file))
+        global json_data
+        json_data = df.to_json(orient='records')
+        return jsonify(json_data)
+    except FileNotFoundError:
+        return jsonify({"error": "Arquivo não encontrado"}), 404
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route("/user", methods=["POST"])
+def create_user(json_data):
+    data = json.loads(json_data)
+    id= data.get('id')
+    name = data.get('name')
+    password = data.get('password')
+
+    new_user = user(id=id, name=name, password=password)
+    db.session.add(new_user)
+    db.session.commit()
 
     return '', 201
+    
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-
--- app/model/user.py
+++ app/model/user.py
@@ -1,7 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from ..app import db
-
-class User(db.Model):
+db = SQLAlchemy(app)
+class user(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     password = db.Column(db.String)
